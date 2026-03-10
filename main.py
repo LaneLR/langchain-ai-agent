0e69e0c57a87790cef468fbcb34ce8e32d235229
LaneLR <laneleerichardson@gmail.com>
2026-03-10 15:22:31 -0500

    set up environment

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
-import langchain
-print("LangChain is ready to go!")
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+def main():
+    print("Hello, World!")
+    
+if __name__ == "__main__":
+    main()
