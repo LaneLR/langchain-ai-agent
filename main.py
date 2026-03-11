ff906993251816e51457261478823646384e53da
LaneLR <laneleerichardson@gmail.com>
2026-03-11 15:32:01 -0500

    update this to work with pydantic

--- main.py
+++ main.py
@@ -1,3 +1,6 @@
+from typing import List
+from langgraph.prebuilt import create_react_agent
+from pydantic import BaseModel, Field
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -12,6 +15,14 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 
 from langchain_tavily import TavilySearch
 
+class Source(BaseModel):
+    """Schema for source used by the agent"""
+    url:str = Field(description="The URL of the source")
+
+class AgentResponse(BaseModel):
+    """Schema for agent response with answer and sources"""
+    answer:str = Field(description="The agent's answer to the query")
+    sources:List[Source] = Field(description="List of sources used by the agent to answer the query")
 
 ### Creating custom tool for tavily search
 
@@ -34,12 +45,20 @@ from langchain_tavily import TavilySearch
 
 
     
-llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash", temperature=0)
+# llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash", temperature=0)
+llm = ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash",
+    project="add_project_id", 
+    location="us-central1",
+    temperature=0,
+    )
+
 tools = [TavilySearch()]
-agent = create_agent(model=llm, tools=tools)
+# llm_with_structure = llm.with_structured_output(AgentResponse)
+agent = create_react_agent(model=llm, tools=tools, response_format=AgentResponse)
 
 def main():
-    result = agent.invoke({"messages":HumanMessage(content="Search for 3 job postings for a junior software engineer using React in Dallas, TX on LinkedIn and posted within the last 30 days")})
+    result = agent.invoke({"messages":HumanMessage(content="Search for 3 job postings for a junior software engineer using React in Dallas, TX on LinkedIn and on Indeed posted within the last 3 days")})
     print(result)
     
 if __name__ == "__main__":
